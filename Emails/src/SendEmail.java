bc1b7edfb1b0b06f80a392b6cae863d2a694616c
nicholas <Nicholas.Thomsen@syntelinc.com>
2019-03-27 14:40:32 -0700

    Email Updates

---
+++ Emails/src/SendEmail.java
@@ -0,0 +1,110 @@
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMultipart;
+
+public class SendEmail {
+
+    public static void main(String[] args) {
+    		
+    	String[] arr = new String[]{"phillipphoong@gmail.com", "hunter.pierce2@knights.ucf.edu", "icebergkj@yahoo.com"};
+    	
+    	
+    	sendBatchEmails(arr);
+
+    }
+    
+    
+    public static void sendIdividualEmail(String email) {
+    	
+        String host="smtp.gmail.com";
+        final String user="";//ENTER YOUR EMAIL!!
+        final String password=""; //ENTER YOUR PASSWORD
+
+        String to=email;
+
+        //imported code
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", user);
+        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(user,password);
+                    }
+                });
+
+        //imported code
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Email Test");
+            message.setText("This email works.");
+
+            message.setSubject("Testing Subject");
+
+            // Create the message part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Now set the actual message
+            messageBodyPart.setText("This is message body");
+
+            // Create a multipart message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "/Users/syntel/Desktop/hello.txt";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            message.setContent(multipart);
+                    Transport.send(message);
+
+            System.out.println("message sent!");
+
+        }
+        catch (MessagingException mex)
+        {
+            System.out.println("Error: unable to send message....");
+            mex.printStackTrace();
+        }
+    }
+    
+    public static void sendBatchEmails(String[] emails) {
+    	
+    	for(int i = 0; i<emails.length; ++i) {
+    		
+    		sendIdividualEmail(emails[i]);
+    	}
+    	
+    }
+}
