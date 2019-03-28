613ea23183102473fdb26ca569a6bf85d2745ce0
irfamustafa <48959671+irfamustafa@users.noreply.github.com>
2019-03-27 23:06:16 -0700

    Add files via upload
    Inserting dummy data into the DB Schema Student_Performance created earlier

---
+++ DBInsertData.sql
@@ -0,0 +1,21 @@
+Insert INTO Users VALUES('IN5', 'Instructor5', 'N');
+Insert INTO Users VALUES('AD2', 'Admin2', 'Y');
+
+Insert INTO Stream VALUES ('FSD123');
+Insert INTO Stream VALUES ('DB343');
+
+Insert INTO Class VALUES ('JV123', 'FSD123', 'IN5');
+
+Insert INTO Employees VALUES('IM505', 'IceMan', 'IM505@syn.com', 'JV123');
+
+Insert INTO Type VALUES('Java', 'FSD123');
+Insert INTO Type VALUES('DB', 'DB343');
+
+Insert INTO Modules VALUES('10', 'JavaMod1', 'Java');
+Insert INTO Modules VALUES('5', 'OracleMod', 'DB');
+
+Insert INTO Courses VALUES('OR1.3', 'Oracle1.3', '5');
+
+Insert INTO Employees_Take_Modules VALUES('10', 'IM505');
+
+Insert INTO Class_Has_Modules VALUES('JV123', '10');
