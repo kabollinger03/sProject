3ab82eb4079a8a177f1596aab20e65f54650955e
irfamustafa <48959671+irfamustafa@users.noreply.github.com>
2019-03-29 11:37:18 -0700

    Add files via upload
    Removed the class_has_modules table so removed those insert statements

--- DBInsertData.sql
+++ DBInsertData.sql
@@ -1,15 +1,12 @@
 Insert INTO Users VALUES('IN5', 'Instructor5', 'N');
 Insert INTO Users VALUES('AD2', 'Admin2', 'Y');
 
-Insert INTO Stream VALUES ('FSD123');
-Insert INTO Stream VALUES ('DB343');
+Insert INTO Stream VALUES ('FSD123', 'Java Full Stack Developer');
+Insert INTO Stream VALUES ('DB343', 'Database');
 
 Insert INTO Class VALUES ('JV123', 'FSD123', 'IN5');
 
 Insert INTO Employees VALUES('IM505', 'IceMan', 'IM505@syn.com', 'JV123');
-
---Insert INTO Type VALUES('Java', 'FSD123');
---Insert INTO Type VALUES('DB', 'DB343');
 
 Insert INTO Modules VALUES('10', 'JavaMod1', 'Java', 'FSD123');
 Insert INTO Modules VALUES('5', 'OracleMod', 'DB', 'DB343');
@@ -17,5 +14,3 @@ Insert INTO Modules VALUES('5', 'OracleMod', 'DB', 'DB343');
 Insert INTO Courses VALUES('OR1.3', 'Oracle1.3', '5');
 
 Insert INTO Employees_Take_Modules VALUES('10', 'IM505', 83.00);
-
-Insert INTO Class_Has_Modules VALUES('JV123', '10');
