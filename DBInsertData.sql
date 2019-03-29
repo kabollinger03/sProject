db54ed13b34de4b0b3a7a71e47cb720950e90401
irfamustafa <48959671+irfamustafa@users.noreply.github.com>
2019-03-29 02:22:32 -0700

    Add files via upload
    Updates in the insert statement: removed the type(i.e. category) insert statements since type(i.e. category) is now just a column in the modules table.

--- DBInsertData.sql
+++ DBInsertData.sql
@@ -8,11 +8,11 @@ Insert INTO Class VALUES ('JV123', 'FSD123', 'IN5');
 
 Insert INTO Employees VALUES('IM505', 'IceMan', 'IM505@syn.com', 'JV123');
 
-Insert INTO Type VALUES('Java', 'FSD123');
-Insert INTO Type VALUES('DB', 'DB343');
+--Insert INTO Type VALUES('Java', 'FSD123');
+--Insert INTO Type VALUES('DB', 'DB343');
 
-Insert INTO Modules VALUES('10', 'JavaMod1', 'Java');
-Insert INTO Modules VALUES('5', 'OracleMod', 'DB');
+Insert INTO Modules VALUES('10', 'JavaMod1', 'Java', 'FSD123');
+Insert INTO Modules VALUES('5', 'OracleMod', 'DB', 'DB343');
 
 Insert INTO Courses VALUES('OR1.3', 'Oracle1.3', '5');
 
