c251927f12032f7b079cb0dbc53cb2744907d313
Chris Mendoza <csmendoza@ucdavis.edu>
2019-03-31 10:30:42 -0700

    removed whitespace and autogenerated comment

--- Module.java
+++ Module.java
@@ -2,58 +2,35 @@ package extraction.sProject;
 
 public class Module {
 	
-	
 	private String moduleID;
 	private double moduelsScore;
 	
-	
-	
 	public Module() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-
-
-
 	public Module(String moduleID, double moduelsScore) {
 		super();
 		this.moduleID = moduleID;
 		this.moduelsScore = moduelsScore;
 	}
 
-
-
+	
 	public String getModuleID() {
 		return moduleID;
 	}
-
-
-
 	public void setModuleID(String moduleID) {
 		this.moduleID = moduleID;
 	}
 
-
-
 	public double getModuelsScore() {
 		return moduelsScore;
 	}
-
-
-
 	public void setModuelsScore(double moduelsScore) {
 		this.moduelsScore = moduelsScore;
 	}
 
-
-
 	@Override
 	public String toString() {
 		return "Module [moduleID=" + moduleID + ", moduelsScore=" + moduelsScore + "]";
-	}
-	
-	
-	
-	
-	
+	}	
 }
