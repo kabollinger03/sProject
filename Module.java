a22d82db428cb433e4544a6c7bfc3847dfbce719
apennella <apennella@albany.edu>
2019-04-01 23:13:47 -0700

    Add files via upload
    Fixed all the issues that were previously mentioned

--- Module.java
+++ Module.java
@@ -2,35 +2,58 @@ package extraction.sProject;
 
 public class Module {
 	
+	
 	private String moduleID;
-	private double moduelsScore;
+	private double moduelScore;
+	
+	
 	
 	public Module() {
 		super();
-	}
-	public Module(String moduleID, double moduelsScore) {
-		super();
-		this.moduleID = moduleID;
-		this.moduelsScore = moduelsScore;
+		// TODO Auto-generated constructor stub
 	}
 
-	
+
+
+	public Module(String moduleID, double moduelScore) {
+		super();
+		this.moduleID = moduleID;
+		this.moduelScore = moduelScore;
+	}
+
+
+
 	public String getModuleID() {
 		return moduleID;
 	}
+
+
+
 	public void setModuleID(String moduleID) {
 		this.moduleID = moduleID;
 	}
 
+
+
 	public double getModuelsScore() {
 		return moduelsScore;
 	}
-	public void setModuelsScore(double moduelsScore) {
-		this.moduelsScore = moduelsScore;
+
+
+
+	public void setModuelScore(double moduelsScore) {
+		this.moduelScore = moduelsScore;
 	}
+
+
 
 	@Override
 	public String toString() {
-		return "Module [moduleID=" + moduleID + ", moduelsScore=" + moduelsScore + "]";
-	}	
+		return "Module [moduleID=" + moduleID + ", moduelScore=" + moduelScore + "]";
+	}
+	
+	
+	
+	
+	
 }
