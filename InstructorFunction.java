454db6e50c914f6daa8749b6a49a7335566f1844
Yue Shen <yue.shen@utexas.edu>
2019-04-03 18:46:59 -0700

    Add files via upload

--- InstructorFunction.java
+++ InstructorFunction.java
@@ -10,22 +10,25 @@ public class InstructorFunction {
 		while (function != 0) {
 			System.out.println("What would you like to do?");
 			System.out.println("Please Enter the number cooresponding to the operation you would like to complete");
-			System.out.println("Enter 0 to exit.");
-			System.out.println("1. Upload PDF");
-			System.out.println("2. Email PDF");
+			System.out.println("Enter 0 to Log Out.");
+			System.out.println("1. Create Class");
+			System.out.println("2. Download PDF");
 			// String function = userIn.nextLine(); // Read user input
 			function = userIn.nextInt();
 			// System.out.println(function);
 
 			switch (function) {
 			case 0:
+				userIn.close();
+				System.out.println("Logging Out");
 				break;
 			case 1:
-				System.out.println("UPLOAD PDF FUNCTIONALITY");
+				System.out.println("CREATE CLASS FUNCTIONALITY");
 				break;
 			case 2:
-				System.out.println("EMAIL PDF FUNCTIONALITY");
+				System.out.println("DOWNLOAD PDF FUNCTIONALITY");
 				break;
+
 			default:
 				System.out.println("No such function, please choose again.");
 			}
