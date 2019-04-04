1b0159923026ba7e535a5bbb677e5588e444d68c
kabollinger03 <48736855+kabollinger03@users.noreply.github.com>
2019-04-03 19:00:08 -0700

    Addin a single point of entry to our demo
    So we can call it and go in. For performence sake, pass a single scanner object. It's safe unless we start doing threads.

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static Scanner stdIn = new Scanner(System.in);
+    
+    public static void main(String[] args){
+        System.out.println("Welcome to the Java demo");
+    }
+}
