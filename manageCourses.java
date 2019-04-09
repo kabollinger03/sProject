9ac65fcdc9f8d74477a133e5c14c2853692f0114
irfamustafa <48959671+irfamustafa@users.noreply.github.com>
2019-04-08 22:55:31 -0700

    User chooses existing stream/module to add course

--- manageCourses.java
+++ manageCourses.java
@@ -35,93 +35,48 @@ public class manageCourses {
 
         if(choice == 1){
             System.out.println("To create a course please provide the information asked for below:");
-            //System.out.println("Please enter the course ID: ");
-            //courseId = userInput.next();
-            
+
             streamId = viewStreams(st);
             moduleId = viewModules(st, streamId);
             
             System.out.println("Please enter the course name: ");
             courseName = userInput.next();
-            
-            //System.out.println("Please enter the module ID: ");
+
             courseId = genCourseId(streamId, moduleId, courseName);
-            
-            System.out.println("GENERATED MODULE ID + " + courseId);
             
             createCourse(st, courseId, courseName, moduleId);
         }
         else if(choice == 2){
             System.out.println("To retrieve or search for a course please provide the information asked for below:");
-            System.out.println("Please enter the course ID: ");
-            courseId = userInput.next();
+            System.out.println("Please enter the course name: ");
+            courseName = userInput.next();
             
-            readCourse(st, courseId);
+            readCourse(st, courseName);
         }
         else if(choice == 3){
             System.out.println("To update the course name please provide the information asked for below:");
-            System.out.println("Please enter the course ID: ");
-            courseId = userInput.next();
             
-            System.out.println("Please enter the course name: ");
+            System.out.println("Please enter the old course name: ");
             courseName = userInput.next();
+            
+            System.out.println("Please enter the new course: ");
+            String courseNewName = userInput.next();
              
-            updateCourse(st, courseId, courseName);
+            updateCourse(st, courseNewName, courseName);
         }
         else if(choice == 4){
             System.out.println("To delete a course please provide the information asked for below:");
-            System.out.println("Please enter the course ID: ");
-            courseId = userInput.next();
+            System.out.println("Please enter the course name: ");
+            courseName = userInput.next();
             
-            deleteCourse(st, courseId);
+            deleteCourse(st, courseName);
         }
         else if(choice == 5){
             return;
         }
     }
-    
-    
-    public manageCourses() {
-        this.courseId = "";
-        this.courseName = "";
-        this.moduleId = "";
-    }
-
-    
-    public manageCourses(String courseId, String courseName, String moduleId) {
-        this.courseId = courseId;
-        this.courseName = courseName;
-        this.moduleId = moduleId;
-    }
-
-    public String getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(String courseId) {
-        this.courseId = courseId;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getModuleId() {
-        return moduleId;
-    }
-
-    public void setModuleId(String moduleId) {
-        this.moduleId = moduleId;
-    }
-    
+        
     public void createCourse(Statement st, String courseId, String courseName, String moduleId){
-        
-        System.out.println("IN CREATE COURSES");
-        
         try{
             st.executeUpdate("INSERT INTO courses VALUES('"+courseId+"', '"+courseName+"', "+moduleId+")");
             System.out.println("course inserted."); //Take this line out if you don't want a confirmation message
@@ -132,31 +87,31 @@ public class manageCourses {
     }
          
         
-    public void readCourse(Statement st, String courseId){
+    public void readCourse(Statement st, String courseName){
         try {
                 System.out.println("Retrieving course");
-                ResultSet rs = st.executeQuery("select course_id, course_name, module_id from Student_Performance.Courses WHERE course_id='" + courseId + "'");
+                ResultSet rs = st.executeQuery("select course_id, course_name, module_id from Student_Performance.Courses WHERE course_name ='" + courseName + "'");
                 while(rs.next())
-                        System.out.println("Result: " + rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+                    System.out.println("Result: " + rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
         } catch (Exception e) {
                 System.out.println("Exception " + e.getMessage());
         }
     }
     
-    public void updateCourse(Statement st, String courseId, String courseName){
+    public void updateCourse(Statement st, String courseNewName, String courseName){
         try {
                 System.out.println("Update a course");
-                st.executeUpdate("UPDATE Student_Performance.Courses SET course_name ='" + courseName + "'WHERE course_id ='" + courseId + "'");
+                st.executeUpdate("UPDATE Student_Performance.Courses SET course_name ='" + courseNewName + "'WHERE course_name ='" + courseName + "'");
                 System.out.println("Course updated");
         } catch (Exception e) {
                 System.out.println("Exception " + e.getMessage());
         }
     }
     
-    public void deleteCourse(Statement st, String courseId){
+    public void deleteCourse(Statement st, String courseName){
         try {
                 System.out.println("Delete course");
-                st.executeUpdate("DELETE FROM Student_Performance.Courses WHERE course_id = '" + courseId + "'");
+                st.executeUpdate("DELETE FROM Student_Performance.Courses WHERE course_Name = '" + courseName + "'");
                 System.out.println("Course deleted");
         } catch (Exception e) {
                 System.out.println("Exception " + e.getMessage());
@@ -179,7 +134,7 @@ public class manageCourses {
                 while(rs.next())
                         System.out.println("Result: " + rs.getString(1) + " " + rs.getString(2));
                 
-                System.out.println("Please choose the Stream for your course to be added into: ");
+                System.out.println("Please choose the Stream ID for your course to be added into: ");
                 streamId = userInput.next();                
         } catch (Exception e) {
                 System.out.println("Exception " + e.getMessage());
@@ -197,17 +152,12 @@ public class manageCourses {
                 while(rs.next())
                         System.out.println("Result: " + rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
                 
-                System.out.println("Please choose the Module for your course to be added into: ");
+                System.out.println("Please choose the Module ID for your course to be added into: ");
                 moduleId = userInput.next();                               
         } catch (Exception e) {
                 System.out.println("Exception " + e.getMessage());
                 moduleId = "";
         }
         return moduleId;
-    }
-    
-    @Override
-    public String toString() {
-        return "manageCourses{" + "courseId=" + courseId + ", courseName=" + courseName + ", moduleId=" + moduleId + '}';
-    }   
+    }  
 }
