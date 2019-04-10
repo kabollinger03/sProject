f5a3cd821bc63ad6d9366359c4d9b93c8d433c9a
gowiredu95 <48763819+gowiredu95@users.noreply.github.com>
2019-04-09 17:10:05 -0700

    Added functionality to create users
    Can now create, update, and delete users. Also includes method to check if User ID already exists before a user is created or updated.

--- main.java
+++ main.java
@@ -1,10 +1,3 @@
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author syntel
